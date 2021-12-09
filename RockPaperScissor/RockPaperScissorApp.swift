bba1c6e9c86946a35821966cf69689d2f875535b
Daniel JCH <danielj.callisayah@gmail.com>
2021-12-09 17:49:20 -0400

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 9/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
